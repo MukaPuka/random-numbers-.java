209144b9126023635911c5770205a0e880f59dc7
MukaPuka <87306217+MukaPuka@users.noreply.github.com>
2021-07-13 07:44:18 +0200

    Add files via upload
    V1

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -1,13 +1,17 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class RandomNumbers{
     public static void main (String[] args){
         int a = (int)(Math.random()*24);
         System.out.println("es ist "+a+" Uhr");
-        int b=18;
-        if (a<b){
+        int b=11, c = 18, d=23, e=4;
+        if (a<b && a>=e){
             System.out.println("Guten Morgen");
-        }else
+        }else if(a>=b && a<c){
+            System.out.println("Guten Tag");
+        }else if (a>=c && a<d){
         System.out.println("GutenAbend");
-
-
+        }else 
+        System.out.println("Gute Nacht");
     }
 }
